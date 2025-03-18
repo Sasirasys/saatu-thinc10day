01e26456261296fe1a6f8597cb969dd042a150f5
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-03-19 00:43:43 +0700

    feat: track PrayStreak

--- app/login/loginFunctions.ts
+++ app/login/loginFunctions.ts
@@ -8,12 +8,12 @@ export async function addUser(email: string, name: string) {
     .select()
     .eq("email", email)
     .single();
-  console.log(user);
+  // console.log(user);
   if (user == null) {
     const { error } = await supabase
       .from("users")
       .insert({ email: email, fullname: name });
-    console.log(error);
+    // console.log(error);
   }
 }
 
@@ -31,6 +31,23 @@ export async function getMyKatha(email: string | null | undefined) {
     .from("katha")
     .select()
     .in("katha_id", [user.saved_katha_id]);
+  const date = new Date();
+  const today = date.toLocaleDateString("en-CA");
+  date.setDate(date.getDate() - 1);
+  const yesterday = date.toLocaleDateString("en-CA");
+  if (yesterday == user.last_login) {
+    // if last login yesterday, +1
+    const { data } = await supabase
+      .from("users")
+      .update({ last_login: today, streak: user.streak + 1 })
+      .eq("email", email);
+  } else if (today != user.last_login) {
+    // if last login not today and not yesterday, reset to 1
+    const { data } = await supabase
+      .from("users")
+      .update({ last_login: today, streak: 1 })
+      .eq("email", email);
+  }
   return { ktlist: kathalist, streak: user.streak };
 }
 
@@ -41,6 +58,6 @@ export async function updateSavedKatha(list: number[], userEmail: string) {
     .update({ saved_katha_id: list })
     .eq("email", userEmail)
     .select();
-  console.log("input:", list);
-  console.log(data, error);
+  // console.log("input:", list);
+  // console.log(data, error);
 }
